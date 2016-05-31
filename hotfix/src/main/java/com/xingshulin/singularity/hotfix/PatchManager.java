b6acb889cafb14ffee185540f680bb6abca7a705
qinhan <qinhan@xingshulin.com>
2016-05-31 18:57:22 +0800

    slightly refactored the code

--- hotfix/src/main/java/com/xingshulin/singularity/hotfix/PatchManager.java
+++ hotfix/src/main/java/com/xingshulin/singularity/hotfix/PatchManager.java
@@ -3,12 +3,15 @@ package com.xingshulin.singularity.hotfix;
 import android.content.Context;
 import com.xingshulin.singularity.core.PatchKt;
 
+import static com.xingshulin.singularity.core.PatchKt.configure;
+import static com.xingshulin.singularity.core.PatchKt.discoverAndApply;
+
 public class PatchManager {
     public static void install(Context context) {
-        PatchKt.configure(context);
+        configure(context);
     }
 
     public static void applyPatch(Context context) {
-        PatchKt.discoverAndApply(context);
+        discoverAndApply(context);
     }
 }
