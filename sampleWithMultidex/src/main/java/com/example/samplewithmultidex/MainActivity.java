c93001708714152250cc18b757ecec16d887798b
hanqin <hanhaify@msn.com>
2016-08-08 18:53:12 +0800

    update sample to add more test cases

--- sampleWithMultidex/src/main/java/com/example/samplewithmultidex/MainActivity.java
+++ sampleWithMultidex/src/main/java/com/example/samplewithmultidex/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.samplewithmultidex;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.welcome)).setText("Default Text");
     }
 }
--- sampleWithMultidex/src/main/java/com/example/samplewithmultidex/SampleMultiDexApplication.java
+++ sampleWithMultidex/src/main/java/com/example/samplewithmultidex/SampleMultiDexApplication.java
@@ -6,6 +6,12 @@ import com.xingshulin.singularity.hotfix.PatchManager;
 
 public class SampleMultiDexApplication extends MultiDexApplication {
     @Override
+    public void onCreate() {
+        super.onCreate();
+        PatchManager.checkForUpdates(this);
+    }
+
+    @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         PatchManager.install(this, "Sk5koHow");
