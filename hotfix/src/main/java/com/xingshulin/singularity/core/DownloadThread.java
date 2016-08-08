c1380e004488af350ad0971e07cb170f078ac3b1
hanqin <hanhaify@msn.com>
2016-08-08 16:51:55 +0800

    save hotfix info after the successful download

--- hotfix/src/main/java/com/xingshulin/singularity/core/DownloadThread.java
+++ hotfix/src/main/java/com/xingshulin/singularity/core/DownloadThread.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import java.io.File;
 import java.net.URL;
 
+import static com.xingshulin.singularity.core.Patch.TAG;
 import static com.xingshulin.singularity.utils.Configs.*;
 import static com.xingshulin.singularity.utils.ContextUtils.appVersionCode;
 import static com.xingshulin.singularity.utils.IOUtils.save;
@@ -26,12 +27,12 @@ class DownloadThread extends Thread {
     @Override
     public void run() {
         String address = format("%s/patches?packageName=%s&appBuild=%s", Patch.DOMAIN, context.getPackageName(), appVersionCode(context));
-        Log.i(Patch.TAG, "fetching hotfix info " + address);
+        Log.i(TAG, "fetching hotfix info " + address);
         String token = getHotfixToken(context);
         JSONObject hotfix = fetchHotfixInfo(address, token);
+        Log.d(TAG, "fetching hotfix info response " + hotfix.toString());
         if (isValid(hotfix) && shouldDownload(hotfix)) {
             doDownload(hotfix, token);
-            saveHotfixInfo(context, hotfix);
         }
     }
 
@@ -39,16 +40,17 @@ class DownloadThread extends Thread {
         try {
             String fileDownloadToken = format("%s/tokens?type=get&key=%s", Patch.DOMAIN, hotfix.getString(Patch.KEY_URI));
             String patchUrl = readText(fileDownloadToken, token);
-            Log.v(Patch.TAG, "downloading patch file " + patchUrl);
+            Log.d(TAG, "downloading patch file " + patchUrl);
             File patchFile = getPatchFile(context);
             save(new URL(patchUrl).openStream(), patchFile);
             if (validatePatch(patchFile, hotfix.getString(Patch.KEY_SHA))) {
-                Log.i(Patch.TAG, "patch file downloaded successfully.");
+                saveHotfixInfo(context, hotfix);
+                Log.i(TAG, "patch file downloaded successfully.");
             } else {
-                Log.w(Patch.TAG, "patch file does not pass sha1 check");
+                Log.w(TAG, "patch file does not pass sha1 check");
             }
         } catch (Exception e) {
-            Log.w(Patch.TAG, e.getMessage(), e);
+            Log.w(TAG, e.getMessage(), e);
         }
     }
 
@@ -57,9 +59,9 @@ class DownloadThread extends Thread {
         try {
             return !hotfix.getString(Patch.KEY_SHA).equalsIgnoreCase(existed.getString(Patch.KEY_SHA));
         } catch (JSONException e) {
-            Log.d(Patch.TAG, e.getMessage(), e);
+            Log.w(TAG, e.getMessage(), e);
         }
-        return false;
+        return true;
     }
 
     private boolean isValid(JSONObject hotfix) {
@@ -71,7 +73,7 @@ class DownloadThread extends Thread {
             JSONArray array = new JSONArray(readText(address, token));
             return array.length() > 0 ? array.getJSONObject(0) : new JSONObject();
         } catch (JSONException e) {
-            Log.w(Patch.TAG, e.getMessage(), e);
+            Log.w(TAG, e.getMessage(), e);
         }
         return new JSONObject();
     }
