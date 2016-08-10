dc094efd77bc38845397bf000dcb6971882a5955
hanqin <hanhaify@msn.com>
2016-08-10 15:15:05 +0800

    remove unused imports in sample

--- sampleWithMultiDex/src/main/java/com/example/samplewithmultidex/MainActivity.java
+++ sampleWithMultiDex/src/main/java/com/example/samplewithmultidex/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.samplewithmultidex;
 
-import android.support.v4.widget.TextViewCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
